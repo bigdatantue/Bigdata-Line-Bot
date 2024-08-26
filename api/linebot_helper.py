0f1a39e9292dd767cbd92f942f875d9b9fd86696
LAPTOP-9DIQETUS\蘇竑翔 <su20050408@gmail.com>
2024-08-26 21:49:39 +0800

    LINEBOTPJ-68 回覆訊息新增Loading Animation 1.在linebot_helper.py新增show_loading_animation_() 2.requirements.txt 需將line-bot-sdk更新為3.11.1

--- api/linebot_helper.py
+++ api/linebot_helper.py
@@ -15,7 +15,8 @@ from linebot.v3.messaging import (
     RichMenuSwitchAction,
     CreateRichMenuAliasRequest,
     QuickReply,
-    QuickReplyItem
+    QuickReplyItem,
+    ShowLoadingAnimationRequest
 )
 import requests
 import random
@@ -37,6 +38,17 @@ class LineBotHelper:
             user_info = line_bot_api.get_profile(user_id)
             return [user_info.display_name, user_info.picture_url]
         
+    @staticmethod
+    def show_loading_animation_(event, time: int=10):
+        """
+        顯示載入動畫
+        """
+        with ApiClient(configuration) as api_client:
+            line_bot_api = MessagingApi(api_client)
+            line_bot_api.show_loading_animation(
+                ShowLoadingAnimationRequest(chatId=event.source.user_id, loadingSeconds=time)
+            )
+
     @staticmethod
     def reply_message(event, messages: list):
         """
